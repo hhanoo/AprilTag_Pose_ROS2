5ddf2861112948d93fd904c0485dfb7ae92a1c4d
hhanoo <woo980711@gmail.com>
2026-04-01 22:06:08 +0900

    [chore] Update Docker run script for improved user environment
    - Add user and group ID to run command for better permissions handling
    - Set environment variables for home directory and ROS logging
    - Mount necessary system directories for enhanced container functionality

--- docker/run.sh
+++ docker/run.sh
@@ -26,9 +26,17 @@ docker run -it --rm \
     --privileged \
     --network host \
     --ipc=host \
-    -e ROS_DOMAIN_ID=99 \
+    --user $(id -u):$(id -g) \
+    -e HOME=/home/developer \
+    -e USER="$(id -un)" \
+    -e LOGNAME="$(id -un)" \
+    -e ROS_LOG_DIR=/tmp/ros_log \
+    -e ROS_DOMAIN_ID=$ROS_DOMAIN_ID \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
+    -v /dev:/dev \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $ROS2_WS_ROOT:/ros2_ws \
     --name $CONTAINER_NAME \
