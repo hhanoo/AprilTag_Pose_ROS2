8776f0f04e343e79737b6d25c1e369df7f9c8277
hhanoo <woo980711@gmail.com>
2026-09-09 12:48:54 +0900

    [chore] Bump all package versions to 3.3.0

--- apriltag_pose_visualizer/setup.py
+++ apriltag_pose_visualizer/setup.py
@@ -7,7 +7,7 @@ setup(
     # Package metadata
     # =========================================================
     name=package_name,
-    version="1.0.0",
+    version="3.3.0",
     # =========================================================
     # Package discovery
     # Automatically find all Python packages in the directory
